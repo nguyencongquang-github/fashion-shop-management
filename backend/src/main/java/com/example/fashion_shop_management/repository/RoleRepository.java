5781613f08d460bc9da7c3b6292bbd1d45eea79c
Nguyễn Công Quang <121441563+nguyencongquang-github@users.noreply.github.com>
2024-11-10 21:58:11 +0700

    Delete backend directory

--- backend/src/main/java/com/example/fashion_shop_management/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.fashion_shop_management.repository;
-
-import com.example.fashion_shop_management.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, String> {
-}
